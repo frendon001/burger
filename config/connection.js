1f5b0ca336f99fdcfffe6c78493cf29c211d4f87
Fausto Rendon <faustor001@gmail.com>
2017-11-24 21:42:00 -0800

    Add heroku db configuration

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host     : 'us-cdbr-iron-east-05.cleardb.net',
+  user     : 'bde157d6419a4b',
+  password : '55b5a6ac',
+  database : 'heroku_8fa0310298c869f'
 });
 
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Portabello Burger', false, '2017-11-20 09:30:00');
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Portabello Burger', true, '2017-11-20 09:30:00');
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('Buffalo Burger', false, '2017-11-20 14:10:00');
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('BBQ Burger', false, '2017-11-20 19:45:00');
